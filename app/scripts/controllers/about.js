513f7ffb9317fd4da13b2eebe0122646b25df589
InvalidPleb <invalidpleb@gmail.com>
2015-10-20 16:41:58 -0600

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name runnerCalcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the runnerCalcApp
+ */
+angular.module('runnerCalcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name runnerCalcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the runnerCalcApp
+ */
+angular.module('runnerCalcApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
