3ae88ed95b879b9111e8d75dc52a15be5d41cbb8
InvalidPleb <invalidpleb1@gmail.com>
2015-10-22 02:30:49 -0600

    Some scripts and labels added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,120 @@
  * Controller of the runnerCalcApp
  */
 angular.module('runnerCalcApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+
+  	$scope.run = '';
+  	$scope.list = [
+	    {
+	      Event: '1Mile',
+	      Title: '1 mile'
+	    }, {
+	      Event: '2km',
+	      Title: '2 km'
+	    }, {
+	      Event: '3km',
+	      Title: '3 km'
+	    }, {
+	      Event: '2Mile',
+	      Title: '2 mile'
+	    }, {
+	      Event: '4km',
+	      Title: '4 km'
+	    }, {
+	      Event: '3Mile',
+	      Title: '3 mile'
+	    }, {
+	      Event: '5kmRoad',
+	      Title: '5 km road'
+	    }, {
+	      Event: '5km',
+	      Title: '5 km'
+	    }, {
+	      Event: '6kmRoad',
+	      Title: '6 km road'
+	    }, {
+	      Event: '6km',
+	      Title: '6 km'
+	    }, {
+	      Event: '4MileRoad',
+	      Title: '4 mile road'
+	    }, {
+	      Event: '4Mile',
+	      Title: '4 mile'
+	    }, {
+	      Event: '8kmRoad',
+	      Title: '8 km road'
+	    }, {
+	      Event: '8km',
+	      Title: '8 km'
+	    }, {
+	      Event: '5MileRoad',
+	      Title: '5 mile road'
+	    }, {
+	      Event: '5Mile',
+	      Title: '5 mile'
+	    }, {
+	      Event: '10kmRoad',
+	      Title: '10 km road'
+	    }, {
+	      Event: '10km',
+	      Title: '10 km'
+	    }, {
+	      Event: '12km',
+	      Title: '12 km'
+	    }, {
+	      Event: '15km',
+	      Title: '15km'
+	    }, {
+	      Event: '10Mile',
+	      Title: '10 mile'
+	    }, {
+	      Event: '20km',
+	      Title: '20 km'
+	    }, {
+	      Event: 'Half.Mar',
+	      Title: 'half mar'
+	    }, {
+	      Event: '25km',
+	      Title: '25 km'
+	    }, {
+	      Event: '30km',
+	      Title: '30 km'
+	    }, {
+	      Event: 'Marathon',
+	      Title: 'marathon'
+	    }, {
+	      Event: '50km',
+	      Title: '50 km'
+	    }, {
+	      Event: '50Mile',
+	      Title: '50 mile'
+	    }, {
+	      Event: '100km',
+	      Title: '100 km'
+	    }, {
+	      Event: '150km',
+	      Title: '150 km'
+	    }, {
+	      Event: '100Mile',
+	      Title: '100 mile'
+	    }, {
+	      Event: '200km',
+	      Title: '200 km'
+	    }
+	];
+
+  	$scope.updateInputDist = function() {
+
+  	};
+
+
+
+  	$scope.updateInputTime = function() {
+
+  	};
+
+
+
+
   });
