c9324a416f1c760270cfa52c0a072d106f0cbf81
InvalidPleb <invalidpleb1@gmail.com>
2015-11-01 22:51:19 -0700

    Angularized old density alt script

--- app/scripts/controllers/densalt.js
+++ app/scripts/controllers/densalt.js
@@ -1,11 +1,87 @@
 'use strict'; 
 
 angular.module('runnerCalcApp')
-  .controller('DensAltCtrl', function () {
+  .controller('DensAltCtrl', function ($scope, $http) {
+
+  	$scope.densButton = function (){
+
+	  	var latUser = 0;
+	    var longUser = 0;
+
+	    navigator.geolocation.getCurrentPosition(GetLocation, err);
+
+	    function GetLocation(location) {
+	        latUser = location.coords.latitude;
+	        longUser = location.coords.longitude;
+
+	        $http({
+	            url:"http://api.wunderground.com/api/74ce66235950b991/geolookup/q/"+latUser+","+longUser+".json",
+	            dataType: 'jsonp' 
+
+	        	}).success(function(json){
+
+	        		var nearestStation = json.location.nearby_weather_stations.pws.station[0].id;
+
+	        		$http({
+
+	        			url:"http://api.wunderground.com/api/74ce66235950b991/conditions/q/pws:"+nearestStation+".json",
+	                    dataType: 'jsonp', 
+	                }).success(function(json){
+
+		        		var pressure = parseInt(json.current_observation.pressure_in);
+		                var alt1 = json.current_observation.observation_location.elevation;
+		                var alt2 = alt1.slice(0,-3);
+		                var altitude = parseInt(alt2);
+		                var oat = json.current_observation.temp_c;
+		                var altM = altitude * 0.3048;
+		                var pStn = (Math.pow(((288 - (altM * 0.0065)) / 288), 5.2561)) * pressure;
+		                var pStnMb = pStn * 33.8639;
+		                var pressureAlt = (1 - (Math.pow((pStnMb / 1013.25), 0.190284))) * 145366.45;
+		                var isa = 15 - (altitude / 500);
+		                var densityAltitude = pressureAlt + (120 * (oat - isa));
+		                $scope.outputDensAlt = densityAltitude;
+		                console.log(densityAltitude);
+
+			        }).error(function(){
+	                    console.log("Error");
+	                });
+
+	        	}).error(function(){
+	        		console.log("Error");
+	        	});
+	    }
+
+	    function err(error) {
+
+	        switch(error.code){
+
+	            case error.PERMISSION_DENIED:
+	                console.log("Access is Denied");
+	                break;
+
+	            case error.POSITION_UNAVAILABLE:
+	                console.log("Position is unavailable");
+	                break; 
+
+	            case error.TIMEOUT:
+	                console.log("Timed Out");
+	                break;
+
+	            case error.UNKNOWN_ERROR:
+	                console.log("Unknown Error");
+	                break;              
+	        }
+	    }
+	};
+
+
+
+
+
+
   	/*
 
-	var latUser = 0;
-    var longUser = 0;
+	
 
     var $this=$(this);
     $this.attr('disabled','disabled');
@@ -16,76 +92,7 @@ angular.module('runnerCalcApp')
 
     }, 3000);
 
+	*/
 
-    navigator.geolocation.getCurrentPosition(GetLocation, err);
 
-    function GetLocation(location) {
-        latUser = location.coords.latitude;
-        longUser = location.coords.longitude;
-
-    //"http://api.wunderground.com/api/74ce66235950b991/geolookup/q/"+latUser+","+longUser+".json"
-    //"http://api.wunderground.com/api/74ce66235950b991/geolookup/q/autoip.json"
-
-        $.ajax({
-             url:"http://api.wunderground.com/api/74ce66235950b991/geolookup/q/"+latUser+","+longUser+".json",
-             dataType: 'jsonp', 
-
-             success:function(json){
-
-                var nearestStation = json.location.nearby_weather_stations.pws.station[0].id;
-
-                $.ajax({
-                    url:"http://api.wunderground.com/api/74ce66235950b991/conditions/q/pws:"+nearestStation+".json",
-                    dataType: 'jsonp', 
-                    success:function(json){
-
-                    var pressure = parseInt(json.current_observation.pressure_in);
-                    var alt1 = json.current_observation.observation_location.elevation;
-                    var alt2 = alt1.slice(0,-3);
-                    var altitude = parseInt(alt2);
-                    var oat = json.current_observation.temp_c;
-                    var altM = altitude * .3048;
-                    var pStn = (Math.pow(((288 - (altM * .0065)) / 288), 5.2561)) * pressure;
-                    var pStnMb = pStn * 33.8639;
-                    var pressureAlt = (1 - (Math.pow((pStnMb / 1013.25), .190284))) * 145366.45;
-                    var isa = 15 - (altitude / 500);
-                    var densityAltitude = pressureAlt + (120 * (oat - isa));
-                    console.log(densityAltitude);
-                
-                     },
-                     error:function(){
-                         console.log("Error");
-                     }      
-                });
-
-             },
-             error:function(){
-                 console.log("Error");
-             }      
-        });
-    }
-
-    function err(error) {
-
-        switch(error.code){
-
-            case error.PERMISSION_DENIED:
-                alert("Access is Denied");
-                break;
-
-            case error.POSITION_UNAVAILABLE:
-                alert("Position is unavailable");
-                break; 
-
-            case error.TIMEOUT:
-                alert("Timed Out");
-                break;
-
-            case error.UNKNOWN_ERROR:
-                alert("Unknown Error");
-                break;              
-        }
-    }
-
-    */
 });
